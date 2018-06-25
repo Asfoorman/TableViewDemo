3ef76f6f325d6f0cef672f2e8afa77c39ff56c60
Anes Mehai <nsamehai@gmail.com>
2018-06-25 13:45:31 -0400

    initial commit

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+    
+    let cellReuseIdentifier = "cell"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    /*
+ 
+ */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier:cellReuseIdentifier) as UITableViewCell!
+        
+        cell.textLabel?.text = self.animals[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell numer \(indexPath.row).")
     }
 
     override func didReceiveMemoryWarning() {
