62b67e45edb17a0524d6570dd67e97b61a7bf0a8
Anes Mehai <nsamehai@gmail.com>
2018-06-25 16:01:23 -0400

    push code so I can use it on other machine. Add images to the list

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -11,8 +11,18 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addAnimal: UIButton!
     
-    let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
+
+    struct Animals
+    {
+        var name: String
+        var image: UIImage
+    }
+    
+    var AnimalsList: [Animals] = [Animals(name: "Horse", image: #imageLiteral(resourceName: "horse")), Animals(name: "Cow", image: #imageLiteral(resourceName: "cow")),
+    Animals(name: "Camel", image: #imageLiteral(resourceName: "camel")), Animals(name: "Sheep", image: #imageLiteral(resourceName: "sheep")), Animals(name: "Goat", image: #imageLiteral(resourceName: "ronaldo"))]
+    
     
     let cellReuseIdentifier = "cell"
     
@@ -27,24 +37,54 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
     }
     
+   
+    @IBAction func doAddAnimal(_ sender: Any) {
+        let alertController = UIAlertController(title: "New Animal",
+                                                message: "Please add your new animal below:",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addTextField(configurationHandler: {(nameField) in
+            nameField.text=""
+            nameField.placeholder="Animal Name:"
+            nameField.isSecureTextEntry=false
+            })
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alertController](_) in
+            let nameField = alertController?.textFields![0]
+            let name = nameField?.text
+            self.AnimalsList.append(Animals(name: name!, image: ))
+            self.tableView.reloadData()
+        }))
+        
+        present(alertController, animated: true, completion: nil)
+    }
     /*
  
  */
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.animals.count
+        return self.AnimalsList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier:cellReuseIdentifier) as UITableViewCell!
         
-        cell.textLabel?.text = self.animals[indexPath.row]
+        cell.textLabel?.text = self.AnimalsList[indexPath.row].name
         
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped cell numer \(indexPath.row).")
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete {
+            AnimalsList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }   else if editingStyle == .insert {
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
